2bfea62f4079646a2b61814c9255114571a7ea9d
IamSmokeY <msdarshankirthic@gmail.com>
2024-09-18 13:58:03 +0530

    added: llama3.1 model card

--- lib/model_configs.py
+++ lib/model_configs.py
@@ -4,6 +4,11 @@ MODELS = {
         'tokenizer_path': 'meta-llama/Llama-2-7b-chat-hf',
         'conversation_template': 'llama-2'
     },
+    'llama3.1': {
+        'model_path': 'meta-llama/Meta-Llama-3.1-8B-Instruct',
+        'tokenizer_path': 'meta-llama/Meta-Llama-3.1-8B-Instruct',
+        'conversation_template': 'llama-2'
+    },
     'vicuna': {
         'model_path': 'lmsys/vicuna-13b-v1.3',
         'tokenizer_path': 'lmsys/vicuna-13b-v1.3',
